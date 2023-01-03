f4f8545defd357506e84b908d074de80f1b14d44
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-01-03 16:08:02 -0500

    commit tree 6056e8d6efe3981e4ee23451100e8e4847c5294b

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
   # write code here
-end
+
+  attr_accessor :s1, :s2, :s3, :kind
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+      #instance variable set equal to an argument
+  end 
+
+  def kind 
+    if  (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= s1 || (s3 + s1) <= s2
+      begin
+        raise TriangleError
+        puts error.message
+      end
+    
+    elsif s1 == s2 && s1 == s3
+      :equilateral
+       
+    elsif s1 == s2 || s1 == s3 || s2 == s3
+      :isosceles  
+
+    else
+      :scalene  
+  end
+ end 
+
+  class TriangleError < StandardError
+    # triangle error code
+    def message 
+      puts "not a triangle"
+    end
+  end 
+end 
+
+ 
